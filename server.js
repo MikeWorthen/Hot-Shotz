637f7205c88c67f146896cb3c84e3d4251ba1754
David Luce <david.luce@outlook.com>
2019-08-05 20:02:24 -0600

    tables created within js

--- server.js
+++ server.js
@@ -1,2 +1,50 @@
+// Reqired 
 var express = require("express");
 var path = require("path");
+
+// server
+var app = express();
+var PORT = 3001;
+
+// app
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// create tables
+
+var table = [
+    {
+        tableNum: "Robert Brownie Jr.",
+        reservedFor: "",
+        name: "",
+        email: "",
+        phone: ""
+    },
+    {
+        tableNum: "Macrelmore",
+        reservedFor: " ",
+        name: "",
+        email: "",
+        phone: ""
+    },
+    {
+        tableNum: "Miley Fryrus",
+        reservedFor: " ",
+        name: "",
+        email: "",
+        phone: ""
+    },
+    {
+        tableNum: "Tom Bready",
+        reservedFor: " ",
+        email: "",
+        phone: ""
+    },
+    {
+        tableNum: "Olvia Fig Newton John",
+        reservedFor: " ",
+        name: "",
+        email: "",
+        phone: ""
+    },
+]

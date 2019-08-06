ee05f41b724476edbf85458bfd01ca68af38bc61
David Luce <david.luce@outlook.com>
2019-08-05 20:35:12 -0600

    server work in progress

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var table = [
     },
     {
         tableNum: "Macrelmore",
-        reservedFor: " ",
+        reservedFor: "",
         name: "",
         email: "",
         phone: ""
@@ -48,3 +48,30 @@ var table = [
         phone: ""
     },
 ]
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/reservationForm.html"));
+});
+app.get("/view", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/reservationViews.html"));
+});
+
+// Displays all characters
+app.get("/api/tables", function (req, res) {
+    return res.json(table);
+});
+
+
+
+
+// // Displays a single character, or returns false
+// app.get("/api/table/:tables", function (req, res) {
+//     var chosen = req.params.character;
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT http://localhost:" + PORT);
+});
